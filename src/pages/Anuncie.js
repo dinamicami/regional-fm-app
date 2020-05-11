34cc5ac4af82fffc9a3cb6ec91a9896af0d601d1
Wellington <wcseven@zoho.com>
2020-05-11 13:51:24 -0300

    Atualizando o expor e tag do anuncie incognito.

--- src/pages/Anuncie.js
+++ src/pages/Anuncie.js
@@ -5,11 +5,12 @@ export default function Anuncie() {
   return(
     <>
       <WebView
+        incognito={true}
         style={{ backgroundColor: '#222' }}
         source={{uri: 'https://radioregionalfm-payment.herokuapp.com/'}}
         allowsBackForwardNavigationGestures={true}
         overScrollMode={'never'}
-        mediaPlaybackRequiresUserAction={false}
+        mediaPlaybackRequiresUserAction={true}
       />
     </>
   )
